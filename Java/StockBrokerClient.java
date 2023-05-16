77c7c7d1b24f238aba4e23ca58e522fc1becd27b
fechan <56131910+fechan@users.noreply.github.com>
2023-05-16 16:02:56 -0700

    add imports for portfolio xml writing

--- Java/StockBrokerClient.java
+++ Java/StockBrokerClient.java
@@ -10,6 +10,12 @@ import java.util.function.Function;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
